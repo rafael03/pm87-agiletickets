b10598549374fdeae87ec3d1cbbfabfcc656582b
Rafael Pereira <rafaelrps11@gmail.com>
2018-11-21 12:35:36 -0200

    Refatorando o código parte 2 - bizu no link: bit.ly/pm87-refactoring

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -9,30 +9,11 @@ public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
 		BigDecimal preco;
-		CalculaPreco evento = null;
-		switch(sessao.getEspetaculo().getTipo()) {
-		case BALLET:
-			evento = new CalculaPrecoBallet();
-			break;
-		case CINEMA:
-			evento = new CalculaPrecoCinema();
-			break;
-		case ORQUESTRA:
-			evento = new CalculaPrecoOquestra();
-			break;
-		case SHOW:
-			evento = new CalculaPrecoShow();
-			break;
-		case TEATRO:
-			evento = new CalculaPrecoTeatro();
-			break;
-		default:
-			break;
+
+		CalculaPreco calculador = sessao.getEspetaculo().getTipo().getCalculadorDePreco();
+		calculador.aplicaPorcentagem(sessao, quantidade);
+		BigDecimal precoUnitario = calculador.getPreco();
 		
-		}
-		
-		evento.aplicaPorcentagem(sessao, quantidade);
-		preco = evento.getPreco();
-		return preco.multiply(BigDecimal.valueOf(quantidade));
+		return precoUnitario.multiply(BigDecimal.valueOf(quantidade));
 	}
 }
--- src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
@@ -1,7 +1,50 @@
 package br.com.caelum.agiletickets.models;
 
+import br.com.caelum.agiletickets.domain.precos.CalculaPreco;
+import br.com.caelum.agiletickets.domain.precos.CalculaPrecoBallet;
+import br.com.caelum.agiletickets.domain.precos.CalculaPrecoCinema;
+import br.com.caelum.agiletickets.domain.precos.CalculaPrecoOquestra;
+import br.com.caelum.agiletickets.domain.precos.CalculaPrecoShow;
+import br.com.caelum.agiletickets.domain.precos.CalculaPrecoTeatro;
+
 public enum TipoDeEspetaculo {
 	
-	CINEMA, SHOW, TEATRO, BALLET, ORQUESTRA;
+	CINEMA {
+		@Override
+		public CalculaPreco calculador() {
+			// TODO Auto-generated method stub
+			return new CalculaPrecoCinema();
+		}
+	}, SHOW {
+		@Override
+		public CalculaPreco calculador() {
+			// TODO Auto-generated method stub
+			return new CalculaPrecoShow();
+		}
+	}, TEATRO {
+		@Override
+		public CalculaPreco calculador() {
+			// TODO Auto-generated method stub
+			return new CalculaPrecoTeatro();
+		}
+	}, BALLET {
+		@Override
+		public CalculaPreco calculador() {
+			// TODO Auto-generated method stub
+			return new CalculaPrecoBallet();
+		}
+	}, ORQUESTRA {
+		@Override
+		public CalculaPreco calculador() {
+			// TODO Auto-generated method stub
+			return new CalculaPrecoOquestra();
+		}
+	};
+	
+	public abstract CalculaPreco calculador();
+	
+	public CalculaPreco getCalculadorDePreco() {
+		return this.calculador();
+	}
 	
 }
